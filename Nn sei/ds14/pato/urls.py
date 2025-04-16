f7dc1de47a5a975906c23d4123454118748e25fd
TamiresSerra <tamiresserra741@gmail.com>
2025-04-16 16:53:00 -0300

    atualizado 16.04.2025

--- Nn sei/ds14/pato/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import PatoListCreateAPIView, PatoDetailAPIView  # Importa a view correta
-
-urlpatterns = [
-    path('pato/', PatoListCreateAPIView.as_view(), name='pato-list-create'),
-    path('patos/<int:pk>/', PatoDetailAPIView.as_view(), name='pato-especifico'),  # Usa a view certa
-]
